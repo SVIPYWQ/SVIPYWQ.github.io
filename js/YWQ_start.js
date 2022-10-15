435c9b12f4c0048fabcde4de21bca24d8fd75a68
SVIPYWQ <wangjiansvip@163.com>
2022-10-16 00:04:21 +0800

    “再次更新信息

--- js/YWQ_start.js
+++ js/YWQ_start.js
@@ -1,6 +1,10 @@
-document.body.insertAdjacentHTML("beforeend", `<div id=particles-js></div>`)
+
 
 function addelement() {
+    document.body.insertAdjacentHTML("beforeend", `<div id=particles-js></div>`)
+    const particles = document.getElementById('particles-js')
+    particles.style.height = "100%"
+
     const aar = ["../js/particles.js", "../js/app.js"]
     const temp = []
     for (let i = 0; i < aar.length; i++) {
